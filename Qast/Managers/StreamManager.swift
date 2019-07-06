2891e8126ec3b7c1a5f27e340d7ca6445f60ad3d
Austin Welch <austinwelch@Austins-MacBook-Pro.local>
2019-07-06 15:12:04 -0400

    Added StreamingManager

---
+++ Qast/Managers/StreamManager.swift
@@ -0,0 +1,9 @@
+//
+//  StreamingManager.swift
+//  Qast
+//
+//  Created by Austin Welch on 7/6/19.
+//  Copyright © 2019 Qast. All rights reserved.
+//
+
+import Foundation
---
+++ Qast/Protocols/TrackManagerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  TrackManagerDelegate.swift
+//  Qast
+//
+//  Created by Austin Welch on 7/6/19.
+//  Copyright © 2019 Qast. All rights reserved.
+//
+
+import Foundation
